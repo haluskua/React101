8c4ec426a736ea739c30b03eeee4b8ff009c3d35
haluskua <haluskua.one@gmail.com>
2020-08-06 16:02:39 +1200

    added extra index file in components to handle all export defaults from components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import image from "./images/expand-vertical-4.svg";
 import { AppContainer } from "./App.styles";
+import { Collapsible } from "./components";
+
 function App() {
   return (
     <AppContainer>
       <header>
         <img src={image} alt="React 101" />
         <h1>Collapsible</h1>
+        <Collapsible />
       </header>
     </AppContainer>
   );
